e8b410f545824cecc8fc4052ed6e13344e3e99f8
Prithivraj200 <prethive200@gmail.com>
2018-10-04 10:08:48 +0530

    dev 0.0.1

---
+++ index.js
@@ -0,0 +1,39 @@
+const cors = require('cors');
+const multer = require('multer');
+const app = require('express')();
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const port = process.env.port || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const storage = multer.diskStorage({
+    destination: 'uploads/',
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({storage}).any();
+
+app.post('/postFile', (req,res) => {
+    upload(req, res, (err) => {
+        if(err) res.status(400).json({msg: 'Error upload file'});
+        else res.status(200).json({msg: 'File uploaded successfully...'})
+    })
+});
+
+app.get('/getFile/:name', (req,res) => {
+    const filename = req.params.name;
+    console.log(filename);
+    fs.readFile(`uploads/${filename}`, (err,data) => {
+       if(err) res.status(400).send(err);
+       else res.status(200).send(data);
+    })
+});
+
+app.listen(port, ()=>{
+    console.log(`Server is running in ${port}...`);
+})
